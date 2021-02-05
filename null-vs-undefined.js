862426ee0de6e1c86e12fe057a70388d24a3964a
AJ Auntor <auntorknocks@gmail.com>
2021-02-05 23:21:49 +0600

    Finish

--- null-vs-undefined.js
+++ null-vs-undefined.js
@@ -1,18 +1,22 @@
-// Here is some Example to get output undefined
-        // let fruit;
-        // console.log(fruit);
+// Here is some Example to get output undefined..........
 
-        // function add(number1, number2){
-        //     console.log(number1 + number2);
-        // }
-        // const result = add(13, 55);
-        // console.log(result);
+    let fruit;
+    console.log(fruit);
 
-        // const student = { Name: "Loe Rock", Phone: +477154 };
-        // console.log(student.MonthlySalary);
+    function add(number1, number2){
+     console.log(number1 + number2);
+        }
+    const result = add(13, 55);
+    console.log(result);
 
-        // const fun = undefined;
-        // console.log(fun);
+    const student = { Name: "Loe Rock", Phone: +477154 };
+    console.log(student.MonthlySalary);
+
+    const fun = undefined;
+    console.log(fun);
+
+
+
 
 
 
