7bda116f7f02b91afbedad3995a89b934ab2af18
AJ Auntor <auntorknocks@gmail.com>
2021-02-05 23:52:25 +0600

    Double and triple equal Exploration

---
+++ double-vs-triple-equal.js
@@ -0,0 +1,49 @@
+// This is true Condition with double equal...................!
+const first = 2;
+const second = "2";
+if (first == second){
+    console.log("This is true condition");
+}
+else{
+    console.log("This is false condition");
+}
+
+const first = 1;
+const second = true;
+if (first == second){
+    console.log("This is true condition");
+}
+else{
+    console.log("This is false condition");
+}
+
+
+// This is false Condition with triple equal...................!
+const first = 5;
+const second = 10;
+if (first === second){
+    console.log("This is true condition");
+}
+else{
+    console.log("This is false condition");
+}
+
+//............................................//
+const first = 5;
+const second = "5";
+if (first === second){
+    console.log("This is true condition");
+}
+else{
+    console.log("This is false condition");
+}
+
+//............................................//
+const first = 1;
+const second = true;
+if (first === second){
+    console.log("This is true condition");
+}
+else{
+    console.log("This is false condition");
+}
