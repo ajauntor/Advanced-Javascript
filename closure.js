25275a465e3e5c0ec685bcffbead7e9b7ce7392a
AJ Auntor <auntorknocks@gmail.com>
2021-02-06 01:37:29 +0600

    Closure encapsulation/ Private variable

---
+++ closure.js
@@ -0,0 +1,20 @@
+function stopWatch(){
+    let count = 0;
+
+    return function(){
+        count++;
+        return count;
+
+    }
+}
+const clockOne = stopWatch();
+console.log(clockOne());
+console.log(clockOne());
+console.log(clockOne());
+console.log(clockOne());
+
+const clockTwo = stopWatch();
+console.log(clockTwo());
+console.log(clockTwo());
+console.log(clockOne());
+console.log(clockTwo());
