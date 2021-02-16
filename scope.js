ad74929153ae9aabae374a125c3556b396535285
AJ Auntor <auntorknocks@gmail.com>
2021-02-16 13:24:38 +0600

    Js Scope Practice

--- scope.js
+++ scope.js
@@ -1,8 +1,8 @@
-function sum (first, second){
-    let result = first + second;
-    console.log(result);
-    return result;
-}
+// function sum (first, second){
+//     let result = first + second;
+//     console.log(result);
+//     return result;
+// }
 
-const output = sum(10, 27);
-console.log(output);
+// const output = sum(10, 27);
+// console.log(output);
