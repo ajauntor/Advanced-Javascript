9670e84fca47b2ac9801d3534887d1c7a72ac1dd
AJ Auntor <auntorknocks@gmail.com>
2021-04-07 13:38:53 +0600

    Null vs Undefind

--- null-vs-undefined.js
+++ null-vs-undefined.js
@@ -10,7 +10,7 @@ const result = add(13, 55);
 console.log(result);
 
 const student = { Name: "Loe Rock", Phone: +477154 };
-// console.log(student.MonthlySalary);
+console.log(student.MonthlySalary);
 
 // const fun = undefined;
 // console.log(fun);
