5bb25adc85e44e46dd35bb039d6cf2bff4814b3c
AJ Auntor <auntorknocks@gmail.com>
2021-04-17 09:02:09 +0600

    null vs undefined

--- null-vs-undefined.js
+++ null-vs-undefined.js
@@ -6,8 +6,8 @@ console.log(fruit);
 function add(number1, number2){
  console.log(number1 + number2);
     }
-const result = add(13, 55);
-console.log(result);
+// const result = add(13, 55);
+// console.log(result);
 
 // const student = { Name: "Loe Rock", Phone: +477154 };
 // console.log(student.MonthlySalary);
