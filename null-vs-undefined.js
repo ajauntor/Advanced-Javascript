888223d40643dbd229490427b13dc0ba2fd3c0ea
AJ Auntor <auntorknocks@gmail.com>
2021-04-26 13:21:08 +0600

    null vs Undefinded Practice

--- null-vs-undefined.js
+++ null-vs-undefined.js
@@ -3,11 +3,11 @@
 let fruit;
 console.log(fruit);
 
-// function add(number1, number2){
-//  console.log(number1 + number2);
-//     }
-// const result = add(13, 55);
-// console.log(result);
+function add(number1, number2){
+ console.log(number1 + number2);
+    }
+const result = add(13, 55);
+console.log(result);
 
 // const student = { Name: "Loe Rock", Phone: +477154 };
 // console.log(student.MonthlySalary);
