99ba2f4f2fdcba546ed359d8848b14a9bb474b13
AJ Auntor <auntorknocks@gmail.com>
2021-04-26 13:20:12 +0600

    null vs Undefinded Practice

--- null-vs-undefined.js
+++ null-vs-undefined.js
@@ -1,5 +1,19 @@
 // Here is some Example to get output undefined..........
 
+let fruit;
+console.log(fruit);
+
+function add(number1, number2){
+ console.log(number1 + number2);
+    }
+const result = add(13, 55);
+console.log(result);
+
+const student = { Name: "Loe Rock", Phone: +477154 };
+console.log(student.MonthlySalary);
+
+// const fun = undefined;
+// console.log(fun);
 
 
 
