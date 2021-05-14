005fe0f7d595b6afd913c5fc856514ec8829dcec
AJ Auntor <auntorknocks@gmail.com>
2021-05-14 09:09:23 +0600

    closure

--- closure.js
+++ closure.js
@@ -1,12 +1,12 @@
 // function stopWatch(){
 //     let count = 0;
 
-//     return function(){
-//         count++;
-//         return count;
+    return function(){
+        count++;
+        return count;
 
-//     }
-// }
+    }
+}
 const clockOne = stopWatch();
 console.log(clockOne());
 console.log(clockOne());
