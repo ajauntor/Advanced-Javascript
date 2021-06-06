edd9d8ac23a6c3aa4e2f91a845a4da4bbdd1ecdd
AJ Auntor <auntorknocks@gmail.com>
2021-06-06 09:27:16 +0600

    conditional

--- condition.js
+++ condition.js
@@ -6,9 +6,9 @@
 
 const age = 50;
 
-if (age) {
-    console.log("Condition is true");
-}
-else {
-    console.log("Condition is false");
-}
+// if (age) {
+//     console.log("Condition is true");
+// }
+// else {
+//     console.log("Condition is false");
+// }
