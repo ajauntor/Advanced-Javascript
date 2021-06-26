34ca0d10e368877699908b8472d1770e9f773f0b
AJ Auntor <auntorknocks@gmail.com>
2021-06-26 09:16:31 +0600

    arraySplit

--- arraySplit.js
+++ arraySplit.js
@@ -1,6 +1,6 @@
 // This is normal slice to split................!
 // This is normal slice to split................!
-// const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 const part = numbers.slice(4, 8);
 console.log(part);
 console.log(numbers);
