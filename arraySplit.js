47e2fc51e53d9b7c044c3598076bcc2c25702601
AJ Auntor <auntorknocks@gmail.com>
2021-06-26 09:17:12 +0600

    arraySplit

--- arraySplit.js
+++ arraySplit.js
@@ -11,6 +11,3 @@ const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 const remove = numbers.splice(2, 6,);
 // console.log(remove);
 // console.log(numbers);
-
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-const part = numbers.slice(4, 8);
