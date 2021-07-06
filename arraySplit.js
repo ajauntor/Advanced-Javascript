d4acd7f444fbcc73c416d55f267e55a483c705c0
AJ Auntor <auntorknocks@gmail.com>
2021-07-06 12:39:13 +0600

    arraySplit

--- arraySplit.js
+++ arraySplit.js
@@ -1,10 +1,24 @@
 // This is normal slice to split................!
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 const part = numbers.slice(4, 8);
-
+console.log(part);
+console.log(numbers);
 
 // // This is splice to split and remove array................!
 // const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 // const remove = numbers.splice(2, 6,);
 // console.log(remove);
 // console.log(numbers);
+
+
+
+// This is normal slice to split................!
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const part = numbers.slice(4, 8);
+
+
+// This is splice to split and remove array................!
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const remove = numbers.splice(2, 6,);
+console.log(remove);
+console.log(numbers);
