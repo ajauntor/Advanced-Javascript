84e53583b0f3eb5e7b08e69da03650c2ba70c097
AJ Auntor <auntorknocks@gmail.com>
2021-07-14 12:53:16 +0600

    break and continue

--- break-continue.js
+++ break-continue.js
@@ -1,6 +1,6 @@
 // This is a break variable....................!
 const nums = [1, 2, 3, 4, 5, 6];
-// for(i = 0; i < nums.length; i++){
+for(i = 0; i < nums.length; i++){
     if(nums[i]>3){
         break;
     }
